3721275d8a30000d886f1068a5c1c10d7d1cba1d
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-10-04 11:50:28 -0400

    agregando soporte para gestión de usuarios con nuevas funciones de obtención, edición y creación utilizando React Query

--- Frontend/src/usuario/api/usuarioPeticion.js
+++ Frontend/src/usuario/api/usuarioPeticion.js
@@ -1,5 +1,16 @@
 import instancia from "../../config/axios";
 
-export const ObtenerUsuarios = async ()=>  instancia.get("/usuario/obtener")
+export const ObtenerUsuarios = async () => {
+  const response = await instancia.get("/usuario/obtener");
+  return response.data;
+}
 
-export const EditarUsuario = async ({ id, body}) => { instancia.patch(`/usuario/actualizar/${id}`, body) }
+export const EditarUsuario = async (id, data) =>{
+  const response = await instancia.put(`/usuario/actualizar/${id}`, data)
+   return response.data
+}
+
+export const AgregarUsuario = async (body) => {
+  const respuesta = await instancia.post('/usuario/agregar', body)
+  return respuesta.data
+}
