e637c68ccd9d32d3d05cd42ba48e231f2a5f547b
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-10-08 23:08:01 -0400

    agregando componentes Modal y FormProvider, junto con el hook useModal para la gestión de formularios y modales

---
+++ Frontend/src/components/modal/Modal.jsx
@@ -0,0 +1,34 @@
+import { motion } from "motion/react";
+export const Modal = ({
+  abierto, cambiarEstado, titulo, children, size = "md"
+}) => {
+
+  if (!abierto) return null
+
+  const sizes = {
+    sm: "max-w-md",
+    md: "max-w-lg",
+    lg: "max-w-2xl",
+    xl: "max-w-4xl"
+  }
+  return (
+    <div
+      onClick={() => cambiarEstado(!abierto)}
+      className="fixed inset-0 backdrop-blur-sm flex items-center 
+      justify-center p-4 z-50">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.9 }}
+        transition={{ duration: 0.2 }}
+        className={`bg-white rounded-xl w-full ${sizes[size]} max-h-[90vh] overflow-y-auto`}
+      >
+        <div className="p-4 sm:p-6">
+          <h2 className="text-lg sm:text-xl font-bold text-gray-800 mb-4">{titulo}</h2>
+          {children}
+        </div>
+      </motion.div>
+
+    </div>
+  )
+}
