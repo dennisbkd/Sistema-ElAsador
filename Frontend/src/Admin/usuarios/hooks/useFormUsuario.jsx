1ba10b198a81b85012d9ca67d1b3f8ab77116500
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-10-08 23:08:36 -0400

    agregando el hook useFormUsuario para gestionar la creación y edición de usuarios con configuración de formulario

---
+++ Frontend/src/Admin/usuarios/hooks/useFormUsuario.jsx
@@ -0,0 +1,32 @@
+import { useMemo } from "react"
+import { useModal } from "../../../hooks/formulario/useModal"
+
+
+export const useFormUsuario = () => {
+  const modal = useModal()
+
+  const guardarUsuario = (datos) => {
+    console.log('Guardando usuario con datos:', datos)
+    if (modal.data) {
+      // editar
+      console.log('Editando usuario:', modal.data)
+    } else {
+      // crear
+      console.log('Creando nuevo usuario', modal.data)
+    }
+  }
+  const configuracionFormulario = useMemo(() => ({
+    defaultValues: {
+      nombre: modal.data?.nombre || '',
+      email: modal.data?.email || '',
+      password: modal.data?.password || '',
+      rol: modal.data?.rol || ''
+    }
+  }), [modal.data])
+
+  return {
+    modal,
+    guardarUsuario,
+    configuracionFormulario
+  }
+}
