0a633fca0e8dbbe84cac00a520b3af0d713b519b
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-10-12 01:02:30 -0400

    agregando un nuevo hook useModal para gestionar el estado y la lógica de apertura y cierre de modales

--- Frontend/src/components/modal/Modal.jsx
+++ Frontend/src/components/modal/Modal.jsx
@@ -13,18 +13,21 @@ export const Modal = ({
   }
   return (
     <div
-      onClick={() => cambiarEstado(!abierto)}
+      onClick={() => cambiarEstado()}
       className="fixed inset-0 backdrop-blur-sm flex items-center 
-      justify-center p-4 z-50">
+      justify-center p-4 z-50 overflow-hidden ">
       <motion.div
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.9 }}
         transition={{ duration: 0.2 }}
-        className={`bg-white rounded-xl w-full ${sizes[size]} max-h-[90vh] overflow-y-auto`}
+        className={`bg-white rounded-xl w-full ${sizes[size]} max-h-[90vh] border border-gray-200 shadow-lg `}
+        onClick={(e) => e.stopPropagation()}
       >
         <div className="p-4 sm:p-6">
-          <h2 className="text-lg sm:text-xl font-bold text-gray-800 mb-4">{titulo}</h2>
+          <h3 className="text-lg sm:text-xl font-bold text-gray-800 mb-4">
+            {titulo}
+          </h3>
           {children}
         </div>
       </motion.div>
