8b3b4f8db46a493366b0b0ae13e8969e2e27b762
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-10-12 01:02:41 -0400

    agregando react-hot-toast y configurando el Toaster en el componente principal; reestructurando rutas de usuarios en AppRouter y eliminando el hook useModal

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router";
 import { SideBarLayout } from "./sideBar/Layout/SideBarLayout";
 import { UsuarioPage } from "./Admin/usuarios/page/UsuarioPage";
+import { UsuarioLayout } from "./Admin/usuarios/layout/UsuarioLayout";
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/home" element={<SideBarLayout />}>
-        <Route path="usuarios" element={<UsuarioPage />} />
+        <Route path="usuarios" element={<UsuarioLayout />}>
+          <Route index element={<UsuarioPage />} />
+        </Route>
       </Route>
     </Routes>
   )
