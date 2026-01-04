6fb1810e25aa45b8d22dfdda223218cd5783029c
dennisbkd <rasguidorondalesdennis@gmail.com>
2026-01-04 01:00:05 -0400

    agregando funciones para la gestión de productos: obtener, crear, editar, eliminar y buscar productos

---
+++ Frontend/src/Admin/gestion-items/producto/api/productoApi.js
@@ -0,0 +1,47 @@
+import { instancia } from "../../../../config/axios"
+
+export const obtenerProductos = async ({ filtroCategoria, page})=>{
+  const limit = 5
+  const offset = (page-1) * limit
+  const res = await instancia.get(`/producto/obtener-productos?offset=${offset}&limit=${limit}&filtroCategoria=${filtroCategoria}`)
+  return res.data
+}
+
+export const crearProducto = async ({data})=>{
+  const res = await instancia.post('/producto/crear-producto', data, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+  return res.data
+}
+
+export const editarProducto = async ({data, id}) => {
+  console.log('Datos para editar el producto:',id)
+  const res = await instancia.put(`/producto/actualizar/${id}`, data, {
+    headers:{
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+  return res.data
+}
+
+export const obtenerProductoId = async (id) => {
+  const res = await instancia.get(`/producto/obtener-producto/${id}`)
+  return res.data
+}
+
+export const eliminarProducto = async (id)=>{
+  const res = await instancia.delete(`/producto/eliminar/${id}`)
+  return res.data
+}
+
+export const busquedaProductoNombre = async (nombre) => {
+  const res = await instancia.get(`/producto/busqueda-producto-nombre?nombre=${nombre}`)
+  return res.data
+}
+
+export const cambiarEstadoProducto = async ({id, activo}) => {
+  const res = await instancia.put(`/producto/cambiar-estado/${id}`, { activo })
+  return res.data
+}
