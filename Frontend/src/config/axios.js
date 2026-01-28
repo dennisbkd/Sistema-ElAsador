e706584d2f2951140cbb559091101656e0205ca0
dennisbkd <rasguidorondalesdennis@gmail.com>
2026-01-28 13:50:51 -0400

    agregando interceptores de solicitud y respuesta en la instancia de axios para manejo de autenticación y redirección en errores

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,5 +1,32 @@
 import axios from "axios";
-export const instancia = axios.create({
+const instancia = axios.create({
   baseURL:import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
   withCredentials: true,
 })
+
+instancia.interceptors.request.use((config)=>{
+  const token = localStorage.getItem('token')
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+},(error)=> Promise.reject(error))
+
+instancia.interceptors.response.use(
+  (response)=>response,
+  (error)=>{
+  if(error.response?.status === 403){
+    window.location.href = '/autorizacion-restringida'
+    return Promise.reject(error)
+  }
+
+  if(error.response?.status === 401 && window.location.pathname !=='/login'){
+    localStorage.removeItem('token')
+    localStorage.removeItem('usuario')
+    window.location.href = '/login'
+  }
+    return Promise.reject(error)
+  }
+)
+
+export {instancia}
