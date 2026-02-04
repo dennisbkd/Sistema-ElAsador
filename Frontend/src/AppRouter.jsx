b22827e6ecd92d5b17f607a11b8bc430d995dd85
dennisbkd <rasguidorondalesdennis@gmail.com>
2026-02-04 15:15:56 -0400

    refactor: eliminar componentes y lógica de Punto de Venta

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -2,10 +2,6 @@ import { Routes, Route, Navigate } from "react-router";
 import { SideBarLayout } from "./sideBar/Layout/SideBarLayout";
 import { UsuarioPage } from "./Admin/usuarios/page/UsuarioPage";
 import { UsuarioLayout } from "./Admin/usuarios/layout/UsuarioLayout";
-// import PuntoVentaUI from "./Admin/punto_de_Venta/PuntoVentaUI";
-// import { PuntoVentaLayout } from "./Admin/punto_de_Venta/layout/puntoVentaLayout";
-// import { PuntoVenta } from "./Admin/punto_de_Venta/page/PuntoVenta";
-// import { ProductoProvider } from "./Admin/punto_de_Venta/context/productoProvider";
 import { CategoriaPage } from "./Admin/gestion-items/categoria/page/CategoriaPage";
 import { CategoriaLayout } from "./Admin/gestion-items/categoria/layout/CategoriaLayout";
 import { ProductoPage } from "./Admin/gestion-items/producto/page/ProductoPage";
@@ -38,14 +34,6 @@ export const AppRouter = () => {
         <Route path="usuarios" element={<UsuarioLayout />} >
           <Route index element={<UsuarioPage />} />
         </Route>
-        {/* <Route path="pedido" element={
-          <ProductoProvider>
-            <PuntoVentaLayout />
-          </ProductoProvider>
-        }>
-          <Route path="listar" element={<PuntoVenta />} />
-        </Route> */}
-        {/* <Route path="punto-de-venta" element={<PuntoVentaUI />} /> */}
         <Route path="productos" element={<ProductoLayout />} >
           <Route index element={<ProductoPage />} />
           <Route path="nuevo" element={<NuevoProductoPage />} />
